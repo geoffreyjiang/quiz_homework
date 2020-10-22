a0acab0351f445b2ae5a8dc0b8d66a56b0bd6108
Geoffrey Jiang <geoffreychjiang@gmail.com>
2020-10-21 23:16:25 -0700

    added questions

---
+++ assets/script.js
@@ -0,0 +1,36 @@
+var questions = [
+
+    {
+        question: " Who won the 2020 NBA finals?"
+        answers: {
+            a: "Miami Heat";
+            b: "Los Angeles Clippers";
+            c: "Los Angeles Lakers";
+        }
+        correctAnswer: "c";
+    };
+
+    {
+        question: " Who did the GOAT aka Kobe Bryant play for? "
+        answers: {
+            a: "Boston Celtics";
+            b: "Los Angeles Lakers";
+            c: "Chicago Bulls";
+        }
+        correctAnswer: "b";
+    }
+
+    {
+        question: 
+
+
+    }
+
+
+
+
+
+
+
+
+]
