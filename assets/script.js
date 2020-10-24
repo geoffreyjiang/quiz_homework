79b18f2e6a6ab59da83b8630747018a8e4dc1f12
Geoffrey Jiang <geoffreychjiang@gmail.com>
2020-10-23 22:20:26 -0700

    finished questions

--- assets/script.js
+++ assets/script.js
@@ -1,42 +1,122 @@
-var timerCount = 120
-var timerEl = document.querySelector("#timer")
-var questionsPointer = 0
-var question = document.querySelector("#question")
-var optionA = document.querySelector("#optionA")
-var optionB = document.querySelector("#optionB")
-var optionC = document.querySelector("#optionC")
-var optionD= document.querySelector("optionD")
-var questions = [ 
-    {
-        q: "Kobe Bryant also known as the greatest player that ever walked the planet played for",
-        choices: ["Los Angeles Clippers", "Boston Celtics", " Los Angeles Lakers", "Chicago Bulls"],
-        answer: "C"
-    },   
-    {
-        q: "From 2000 - 2020, who won the most NBA championships",
-        pick: ["Golden State Warriors", "San Antonio Spurs", "Miami Heat", "Los Angeles Lakers"],
-        answer: "D",
-    },
-    {
-        q: "Who won a NBA Championship during a pandemic",
-        pick: ["Miami Heat", "Los Angeles Lakers", "Los Angeles Clippers", "Toronto Raptors"],
-        answer: "B"
-    },
-    {
-        q: "Whoever made this quiz favorite NBA team must be",
-        pick: ["Los Angeles Lakers", "Los Angeles Clippers", "Portland Trailblazers", "Miami Heat"],
-        answer: "A"
-    }
+var timerCount = 120;
+var timerEl = document.querySelector("#timer");
+var questionsPointer = 0;
+var question = document.querySelector("#question");
+var optionA = document.querySelector("#optionA");
+var optionB = document.querySelector("#optionB");
+var optionC = document.querySelector("#optionC");
+var optionD = document.querySelector("#optionD");
+var startBtn = document.querySelector("#startBtn");
+
+timerEl.textContent = timerCount;
+startBtn.addEventListener("click", function () {
+    startBtn.style.visibility = "false";
+    var timer = window.setInterval(function () {
+    timerCount--;
+    timerEl.textContent = timerCount;
+
+    if (timerCount === 0) {
+        clearInterval(timer);
+    }}, 1000);
+    askQuestions();
+});
+
+var questions = [
+{
+    q:
+        "Kobe Bryant also known as the greatest player that ever walked the planet played for",
+    choices: [
+        "Los Angeles Clippers",
+        "Boston Celtics",
+        "Los Angeles Lakers",
+        "Chicago Bulls"
+    ],
+    a: "C"
+},
+{
+    q: 
+        "From 2000 - 2020, who won the most NBA championships",
+    choices: [
+        "Golden State Warriors",
+        "San Antonio Spurs",
+        "Miami Heat",
+        "Los Angeles Lakers"
+    ],
+    a: "D"
+},
+{
+    q: 
+        "Who won a NBA Championship during a pandemic",
+    choices: [
+        "Miami Heat",
+        "Los Angeles Lakers",
+        "Los Angeles Clippers",
+        "Toronto Raptors"
+    ],
+        a: "B"
+},
+{
+    q: "Whoever made this quiz favorite NBA team must be",
+    choices: [
+        "Los Angeles Lakers",
+        "Los Angeles Clippers",
+        "Portland Trailblazers",
+        "Miami Heat"
+    ],
+    a: "A"
+}
 ];
 
-timerEl.textContent = timerCount
-
-var timer = window.setInterval(function () {
-timerCount--;
-timerEl.textContent = timerCount
-
-if (timerCount === 0) {
-    clearInterval(timer)
+function askQuestions() {
+    if (questionsPointer === questions.length) {
+    clearInterval(timer);
+    alert("You finished!");
+    return;
 }
-}, 1000)
+    question.textContent = questions[questionsPointer].q;
+    optionA.textContent = questions[questionsPointer].choices[0];
+    optionB.textContent = questions[questionsPointer].choices[1];
+    optionC.textContent = questions[questionsPointer].choices[2];
+    optionD.textContent = questions[questionsPointer].choices[3];
+}
 
+optionA.addEventListener("click", function () {
+    if (optionA.getAttribute("data-answer") === questions[questionsPointer].a) {
+        alert("Correct!");
+        questionsPointer++;
+        askQuestions();
+}    else {
+    alert("Wrong!");
+    timerCount -= 10;
+}
+});
+optionB.addEventListener("click", function () {
+    if (optionB.getAttribute("data-answer") === questions[questionsPointer].a) {
+        alert("Correct!");
+        questionsPointer++;
+        askQuestions();
+}   else {
+        alert("Wrong!");
+        timerCount -= 10;
+}
+});
+optionC.addEventListener("click", function () {
+    if (optionC.getAttribute("data-answer") === questions[questionsPointer].a) {
+        alert("Correct!");
+        questionsPointer++;
+        askQuestions();
+}   else {
+        alert("Wrong!");
+        timerCount -= 10;
+}
+optionD.addEventListener("click", function () {
+    if (optionD.getAttribute("data-answer") === questions[questionsPointer].a) {
+        alert("Correct!");
+        questionsPointer++;
+        askQuestions();
+}   else {
+        alert("Wrong!");
+        timerCount -= 10;
+}
+});
+});
